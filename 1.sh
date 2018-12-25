5bc43316b91dede1804b4dfacd30cb696763af5c
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-25 09:53:57 +0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#echo "Enter teamviewer password: "
+#read TIMPASS
+
+useradd -G sudo -d /home/bot -m -s /bin/bash bot
+passwd bot
+
+sudo apt update
+sudo apt install xubuntu-core xrdp -y
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+chmod 777 ./teamviewer_amd64.deb
+apt install ./teamviewer_amd64.deb -y
+teamviewer passwd qazwsxedc
+ping 8.8.8.8 -c 10
+teamviewer license accept
+teamviewer info | grep teamviewer ID
+read -n 1 -s -r -p "your password: qazwsxedc        Copy Teamviewer ID   and Press any key to continue"
+reboot
