41d68450fe8e4ee6d9695f56577a2fdefd07eba8
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-05-17 01:47:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@ HERE
 
 docker-compose --file /opt/matrix/docker-compose.yml up -d
 sleep 10
-echo "Please create ferst admin-user:"
+#echo "Please create ferst admin-user:"
 docker exec -it synapse register_new_matrix_user -u admin -p $ADMIN_PASS -a -c /data/homeserver.yaml http://localhost:8008
 
 echo "#####################################################################################################################"
