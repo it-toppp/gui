05411d332d1794df5f177f084a36409bd524d037
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-05-20 13:28:10 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -19,11 +19,11 @@ gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 HERE
 
-systemctl stop mariadb 
-yum remove mariadb mariadb-server -y
-yum install MariaDB-server MariaDB-client -y
-systemctl start mariadb && systemctl enable mariadb
-mysql_upgrade
+systemctl stop mariadb 1>/dev/null
+yum remove mariadb mariadb-server -y 1>/dev/null
+yum install MariaDB-server MariaDB-client -y 1>/dev/null
+systemctl start mariadb && systemctl enable mariadb  1>/dev/null
+mysql_upgrade 1>/dev/null
 
 cat >>/etc/my.cnf << HERE 
 performance-schema=0
@@ -41,7 +41,7 @@ systemctl restart mariadb
 
 yum install ffmpeg ffmpeg-devel nano mc htop atop iftop lsof bzip2 traceroute gdisk php74-php-curl php74-php-mbstring  php74-php-xml php74-php-gd php74-php-fileinfo php74-php-exif php74-php-intl php74-php-zip php74-php-mysqli php74-php-curl php74-php-ctype php74-php-openssl php74-php-pdo php74-php-opcache php74-php-simplexml php74-php-mysql php72-php-mbstring php72-php-xml php72-php-gd php72-php-fileinfo php72-php-intl php72-php-zip php72-php-mysqli php72-php-curl php72-php-ctype php72-php-openssl php72-php-pdo php72-php-exif php72-php-opcache php72-php-simplexml php72-php-mysql php72-php-curl php74-php-xdebug php73-php-xdebug php72-php-xdebug php70-php-xdebug php72-php-soap php73-php-soap php74-php-soap -y &> /dev/null
 
-wget https://raw.githubusercontent.com/Skamasle/sk-php-selector/master/sk-php-selector2.sh && chmod +x sk-php-selector2.sh && bash sk-php-selector2.sh php70 php71 php72 php73
+wget https://raw.githubusercontent.com/Skamasle/sk-php-selector/master/sk-php-selector2.sh && chmod +x sk-php-selector2.sh && bash sk-php-selector2.sh php70 php71 php72 php73 &> /dev/null
 
 cat >>/etc/httpd/conf.d/fcgid.conf << HERE 
 FcgidBusyTimeout 72000
