328da8adca98ac1e1872370559606d525357c02f
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-06-01 13:16:07 +0300

    Update vestacp.sh

--- vestacp.sh
+++ vestacp.sh
@@ -156,10 +156,15 @@ sed -i 's|$BIN/v-check-vesta-license|#$BIN/v-check-vesta-license|' /usr/local/ve
 
 echo "Fix VESTACP-FileManager successfully"
 
-rpm -Uvh http://www.city-fan.org/ftp/contrib/yum-repo/rhel7/x86_64/city-fan.org-release-2-1.rhel7.noarch.rpm -y 1>/dev/null
-yum -y --enablerepo=city-fan.org install libcurl libcurl-devel -y 1>/dev/null
-yum -y group install 'Development Tools' -y 1>/dev/null
-yum  install perl-core zlib-devel -y 1>/dev/null
+cat >/etc/yum.repos.d/city-fan.repo << HERE 
+[CityFan]
+name=City Fan Repo
+baseurl=http://www.city-fan.org/ftp/contrib/yum-repo/rhel$releasever/$basearch/
+enabled=1
+gpgcheck=0
+HERE
+
+yum install curl -y 1>/dev/null
 
 echo "Full installation completed [ OK ]"
 
