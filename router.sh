d5ae89477931e16a685296fe8ee0b7b59a7ba0e1
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-06-04 01:16:58 +0300

    Create router.sh

---
+++ router.sh
@@ -0,0 +1,5 @@
+
+WAN (eth0) – this interface will get an IP from the ISP, so we leave it using DHCP.
+
+LAN (eth1) – we configure the interface with a static IP within the subnet we are going to use for local area network
+apt-get install isc-dhcp-server
