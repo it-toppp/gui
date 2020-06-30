7273bf805e0cf7d19f3b87e4f2607d867fc36204
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-06-30 16:45:33 +0300

    Update vestacp.sh

--- vestacp.sh
+++ vestacp.sh
@@ -63,7 +63,10 @@ yum -y install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 yum -y install nodejs
 
-yum install ffmpeg ffmpeg-devel nano mc htop atop iftop lsof bzip2 traceroute gdisk php74-php-curl php74-php-mbstring  php74-php-xml php74-php-gd php74-php-fileinfo php74-php-exif php74-php-intl php74-php-zip php74-php-mysqli php74-php-curl php74-php-ctype php74-php-openssl php74-php-pdo php74-php-opcache php74-php-simplexml php74-php-mysql php72-php-mbstring php72-php-xml php72-php-gd php72-php-fileinfo php72-php-intl php72-php-zip php72-php-mysqli php72-php-curl php72-php-ctype php72-php-openssl php72-php-pdo php72-php-exif php72-php-opcache php72-php-simplexml php72-php-mysql php72-php-curl php74-php-xdebug php73-php-xdebug php72-php-xdebug php70-php-xdebug php72-php-soap php73-php-soap php74-php-soap -y &> /dev/null
+yum install ffmpeg ffmpeg-devel nano git mc htop atop iftop lsof bzip2 traceroute gdisk -y &> /dev/null
+yum install php74-php-curl php74-php-mbstring  php74-php-xml php74-php-gd php74-php-fileinfo php74-php-exif php74-php-intl php74-php-zip php74-php-mysqli php74-php-curl php74-php-ctype php74-php-openssl php74-php-pdo php74-php-opcache php74-php-simplexml php74-php-mysql php74-php-soap php74-php-xdebug -y &> /dev/null
+yum install php72-php-mbstring php72-php-xml php72-php-gd php72-php-fileinfo php72-php-intl php72-php-zip php72-php-mysqli php72-php-curl php72-php-ctype php72-php-openssl php72-php-pdo php72-php-exif php72-php-opcache php72-php-simplexml php72-php-mysql php72-php-curl php72-php-xdebug php72-php-soap -y &> /dev/null
+yum install php73-php-curl php73-php-mbstring  php73-php-xml php73-php-gd php73-php-fileinfo php73-php-exif php73-php-zip php73-php-mysqli php73-php-curl php73-php-ctype php73-php-openssl php73-php-soap php73-php-intl php73-php-pdo php73-php-opcache php73-php-simplexml php73-php-mysql php73-php-xdebug -y &> /dev/null
 
 wget https://raw.githubusercontent.com/it-toppp/sk-php-selector/master/sk-php-selector2.sh &> /dev/null
 chmod +x sk-php-selector2.sh && bash sk-php-selector2.sh php70 php71 php72 php73 &> /dev/null
@@ -194,6 +197,11 @@ gpgcheck=0
 HERE
 
 yum install curl -y 1>/dev/null
+
+#EXIM
+wget https://ca1.dynanode.net/exim-4.93-3.el7.x86_64.rpm
+rpm -Uvh --oldpackage exim-4.93-3.el7.x86_64.rpm
+systemctl restart exim
 
 echo "Full installation completed [ OK ]"
 
