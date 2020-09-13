53b7a5c30b6f65a96c9889e164baaee0e0d4c03a
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-09-13 22:51:23 +0300

    Update hestiacp.sh

--- hestiacp.sh
+++ hestiacp.sh
@@ -1,4 +1,20 @@
-cat >>  /etc/php/7.2/fpm/php.ini /etc/php/7.4/fpm/php.ini /etc/php/7.1/fpm/php.ini << HERE 
+#!/bin/bash
+
+PASSWD=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= < /dev/urandom | head -c 12)
+DOMAIN=$1
+IP=$(curl ifconfig.me)
+DIG_IP=$(getent ahostsv4 $DOMAIN | sed -n 's/ *STREAM.*//p')
+
+echo $DOMAIN
+echo $PASSWDD
+hostnamectl set-hostname $DOMAIN
+wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
+sudo bash hst-install.sh --apache yes --nginx yes --phpfpm yes --multiphp yes --vsftpd yes --proftpd no --named yes --mysql yes --postgresql no --exim yes --dovecot yes --clamav no --spamassassin yes --iptables yes --fail2ban yes --quota no --api yes --force no --interactive no --port 8083 --hostname $DOMAIN --email admin@$DOMAIN --password $PASSWD --lang en
+
+echo Installation will take about 5 minutes ...
+
+#PHP
+cat >> /etc/php/7.4/fpm/php.ini << HERE 
 file_uploads = On
 allow_url_fopen = On
 post_max_size = 5120M
@@ -10,12 +26,40 @@ max_input_time = 6000
 zlib.output_compression = Off
 memory_limit = 1000M
 HERE
+echo "Fix PHP successfully"
 
-echo "Fix PMA successfully"
+cat >> /etc/php/7.3/fpm/php.ini << HERE 
+file_uploads = On
+allow_url_fopen = On
+post_max_size = 5120M
+upload_max_filesize = 5120M
+output_buffering = Off
+max_execution_time = 6000
+max_input_vars = 3000
+max_input_time = 6000
+zlib.output_compression = Off
+memory_limit = 1000M
+HERE
+echo "Fix PHP successfully"
+
+cat >>  /etc/php/7.2/fpm/php.ini << HERE 
+file_uploads = On
+allow_url_fopen = On
+post_max_size = 5120M
+upload_max_filesize = 5120M
+output_buffering = Off
+max_execution_time = 6000
+max_input_vars = 3000
+max_input_time = 6000
+zlib.output_compression = Off
+memory_limit = 1000M
+HERE
+echo "Fix PHP successfully"
+
 #nginx
-sed -i 's|client_max_body_size            256m|client_max_body_size            5120m|' /etc/nginx/nginx.conf
+sed -i 's|client_max_body_size            256m|client_max_body_size  5120m|' /etc/nginx/nginx.conf
 sed -i 's|worker_connections  1024;|worker_connections  2024;|' /etc/nginx/nginx.conf
-sed -i 's|send_timeout                    60;|send_timeout                    3000;|' /etc/nginx/nginx.conf
+sed -i 's|send_timeout                    60;|send_timeout  3000;|' /etc/nginx/nginx.conf
 sed -i 's|proxy_connect_timeout           30|proxy_connect_timeout   9000|' /etc/nginx/nginx.conf
 sed -i 's|proxy_send_timeout              180|proxy_send_timeout  9000|' /etc/nginx/nginx.conf
 sed -i 's|proxy_read_timeout              300|proxy_read_timeout  9000|' /etc/nginx/nginx.conf
@@ -29,3 +73,4 @@ echo "Fix MYSQL successfully"
 
 
 apt -y install ffmpeg
+apt-get install -y nodejs
