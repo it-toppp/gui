f408104808c006487357b62b0fc09ee551ad0920
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-09-18 07:29:41 +0300

    Update myvesta.sh

--- myvesta.sh
+++ myvesta.sh
@@ -128,7 +128,7 @@ read -p "Script [1]: " script
     case "$script" in
 1|"")
 cd /home/admin/web/$DOMAIN/public_html
-wget http://ss.ultahost.com/playtube.zip && unzip playtube.zip && chmod -R 777 config.php upload assets/import/ffmpeg/ffmpeg nodejs/config.json
+wget http://ss.ultahost.com/playtube.zip && unzip playtube.zip &> /dev/null && chmod -R 777 config.php upload assets/import/ffmpeg/ffmpeg nodejs/config.json
 rm -Rfv __MACOSX playtube.zip robots.txt index.html &> /dev/null
 sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 chown -R admin:admin /home/admin/web
@@ -136,7 +136,7 @@ echo "  installation complete"
 ;;
 2)
 cd /home/admin/web/$DOMAIN/public_html
-wget http://ss.ultahost.com/wowonder.zip && unzip wowonder.zip &> /dev/null&& chmod -R 777 cache upload config.php && chown -R admin:admin ./
+wget http://ss.ultahost.com/wowonder.zip && unzip wowonder.zip &> /dev/null && chmod -R 777 cache upload config.php && chown -R admin:admin ./
 rm -Rfv __MACOSX wowonder.zip robots.txt index.html &> /dev/null
 sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 echo "  installation complete"
@@ -150,7 +150,7 @@ echo "  installation complete"
 ;;
 4)
 cd /home/admin/web/$DOMAIN/public_html
-wget http://ss.ultahost.com/quickdate.zip && unzip quickdate.zip robots.txt index.html && chmod -R 777 upload cache config.php ffmpeg/ffmpeg && chown -R admin:admin ./
+wget http://ss.ultahost.com/quickdate.zip && unzip quickdate.zip &> /dev/null && chmod -R 777 upload cache config.php ffmpeg/ffmpeg && chown -R admin:admin ./
 rm -Rfv __MACOSX quickdate.zip robots.txt index.html &> /dev/null
 sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 echo "  installation complete"
