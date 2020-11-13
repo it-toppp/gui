02f8c24b21d2bdca43b7b88700446860e6fea981
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-11-13 06:39:04 +0300

    Update hestiacp.sh

--- hestiacp.sh
+++ hestiacp.sh
@@ -69,7 +69,7 @@ systemctl restart  mysql 1>/dev/null
 echo "Fix MYSQL successfully"
 
 #PHP
-grep -rl  "pm.max_children = 8" /etc/php /usr/local/vesta/data/templates/web/ | xargs perl -p -i -e 's/pm.max_children = 8/pm.max_children = 100/g'
+grep -rl  "pm.max_children = 8" /etc/php /usr/local/hestia/data/templates/web | xargs perl -p -i -e 's/pm.max_children = 8/pm.max_children = 100/g'
 
 cat >> /etc/php/7.4/fpm/php.ini << HERE 
 file_uploads = On
