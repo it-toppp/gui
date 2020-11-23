660940e14b02b3d1a62bd1ed9a1eee9985391f3b
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-11-23 22:32:26 +0300

    Update vesta-change-ip.sh

--- vesta-change-ip.sh
+++ vesta-change-ip.sh
@@ -3,9 +3,12 @@
 #IPNEW=$2
 IPOLD=$(ls /usr/local/*/data/ips)
 IPNEW=$(wget -O - -q ifconfig.me)
-grep -rl $1 /etc /usr/local/ /home/*/conf | xargs perl -p -i -e 's/$1/$2/g' 
-mv /usr/local/*/data/ips/$1 /usr/local/*/data/ips/$2
-mv /etc/apache2/conf.d/$1.conf /etc/apache2/conf.d/$2.conf
-mv /etc/nginx/conf.d/$1.conf /etc/nginx/conf.d/$2.conf
+grep -rl $IPOLD /etc /usr/local/ /home/*/conf | xargs perl -p -i -e 's/$IPOLD/$IPNEW/g' 
+if [ ! -d " /etc/httpd" ]; then
+    mv /etc/apache2/conf.d/$IPOLD.conf /etc/apache2/conf.d/$IPNEW.conf
+fi
+mv /usr/local/*/data/ips/$IPOLD /usr/local/*/data/ips/$IPNEW
+mv /etc/httpd/conf.d/$IPOLD.conf /etc/httpd/conf.d/$IPNEW.conf
+mv /etc/nginx/conf.d/$IPOLD.conf /etc/nginx/conf.d/$IPNEW.conf
 v-rebuild-user admin
 echo changed
